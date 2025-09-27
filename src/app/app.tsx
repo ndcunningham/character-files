44936f1f944270356aa4c02041fe3858943bda1a
ndcunningham <ndcunningham@gmail.com>
2025-09-27 14:21:51 -0600

    feat(app): add sample files and base scss

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,53 @@
 // Uncomment this line to use CSS modules
+import FileDownload from 'src/components/FileDownload';
+import { FileItem } from '../types/file-item';
 import styles from './app.module.scss';
+  
+
+const sampleFiles: FileItem[] = [
+  {
+    name: 'smss.exe',
+    device: 'Mario',
+    path: '\\Device\\HarddiskVolume2\\Windows\\System32\\smss.exe',
+    status: 'scheduled'
+  },
+  {
+    name: 'netsh.exe',
+    device: 'Luigi',
+    path: '\\Device\\HarddiskVolume2\\Windows\\System32\\netsh.exe',
+    status: 'available'
+  },
+  {
+    name: 'uxtheme.dll',
+    device: 'Peach',
+    path: '\\Device\\HarddiskVolume1\\Windows\\System32\\uxtheme.dll',
+    status: 'available'
+  },
+  {
+    name: 'aries.sys',
+    device: 'Daisy',
+    path: '\\Device\\HarddiskVolume1\\Windows\\System32\\aries.sys',
+    status: 'scheduled'
+  },
+  {
+    name: 'cryptbase.dll',
+    device: 'Yoshi',
+    path: '\\Device\\HarddiskVolume1\\Windows\\System32\\cryptbase.dll',
+    status: 'scheduled'
+  },
+  {
+    name: '7za.exe',
+    device: 'Toad',
+    path: '\\Device\\HarddiskVolume1\\temp\\7za.exe',
+    status: 'scheduled'
+  }
+];
 
 export function App() {
   return (
-    <div>
-
+    <div className={styles.app}>
+      <h1>Character Downloads</h1>
+      <FileDownload files={sampleFiles} />
     </div>
   );
 }
